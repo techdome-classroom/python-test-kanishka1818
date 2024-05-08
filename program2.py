636c1f93b442330523a5ed61c77757d24df12244
kanishka padamata <99426148+kanishka1818@users.noreply.github.com>
2024-05-08 10:59:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ def decode_message( s: str, p: str) -> bool:
         if len(s)!=len(p) :
                 if (p =='*'):
                         return True
-                for i in range(len(s))
-                if(p=='?')
+                for i in range(len(s)):
+                    
   
         return False
